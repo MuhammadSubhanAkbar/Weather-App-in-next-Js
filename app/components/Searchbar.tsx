fe3e31a6e5072698f427d73ecb23559ced6fee17
Subhan Akbar <subhanakbar346@gmail.com>
2025-11-21 23:16:55 +0500

    Still making NavBar

--- app/components/Searchbar.tsx
+++ app/components/Searchbar.tsx
@@ -1,24 +1,31 @@
 import { IoSearch } from "react-icons/io5";
+import { ChangeEventHandler, FormEventHandler } from "react";
 
 type Props = {
     value: string;
-    onchange: (value: string) => void;
+    onChange: ChangeEventHandler<HTMLInputElement> | undefined;
+    onSubmit: FormEventHandler<HTMLFormElement> | undefined;
 }
 
-export default function Searchbar() {
+export default function Searchbar(props: Props) {
     return (
         <div>
-            <form className="flex relative items-center justify-center h-10">
+            <form
+                onSubmit={props.onSubmit}
+                className="flex relative items-center justify-center h-10"
+            >
                 <input
                     type="text"
                     placeholder="Search"
-                    className="px-4 py-4 w-[230] border border-gray-300 round-l-md focus:outline-none
+                    value={props.value}
+                    className="px-4 py-4 w-[230px] border border-gray-300 rounded-l-md focus:outline-none
                     focus:border-blue-500 h-full"
+                    onChange={props.onChange}
                 />
                 {/*Creating button for search*/}
                 <button className="px-4 py-5 bg-blue-500 text-white rounded-r-md focus:border-blue-500
-                    focus:outline-none hover-blue-600 h-full">
-                    <IoSearch  />
+                    focus:outline-none hover:bg-blue-600 h-full">
+                    <IoSearch />
                 </button>
             </form>
         </div>
